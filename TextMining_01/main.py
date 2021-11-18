c0d2cfebf558a3006de62218578b9567b86d26d8
s363289 <tobias.brandner@stud-mail.uni-wuerzburg.de>
2021-11-18 16:56:25 +0100

    Update from 17.11.21, Trigram Counting and Likelihood rating works

--- TextMining_01/main.py
+++ TextMining_01/main.py
@@ -6,6 +6,9 @@
 import nltk
 import math
 from nltk import ngrams
+from nltk.tokenize import RegexpTokenizer
+
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
@@ -27,9 +30,28 @@ def binom(k, n, p) -> float:
     return math.pow(p, k) * math.pow((1 - p), n - k)
 
 
-def log_lambda(n, c1, c2, c3, p, p1, p2) -> float:
-    return math.log(binom(c3, c1, p)) + math.log(binom(c2 - c3, n - c1, p)) - math.log(binom(c3, c1, p1)) \
-           - math.log(binom(c2 - c3, n - c1, p2))
+def log_lambda(n, c12, c123, c3, p, p1, p2) -> float:
+    # if(n == 0 or c12 == 0 or c2 == 0 or c3 == 0 or p == 0 or p1 == 0 or p2 == 0):
+    #     return 0
+
+    b1 = binom(c123, c12, p)
+    b2 = binom(c3 - c123, n - c12, p)
+    b3 = binom(c123, c12, p1)
+    b4 = binom(c3 - c123, n - c12, p2)
+    log1 = 0
+    if b1 != 0:
+        log1 = math.log(b1)
+    log2 = 0
+    if b2 != 0:
+        log2 = math.log(b2)
+    log3 = 0
+    if b3 != 0:
+        log3 = math.log(b3)
+    log4 = 0
+    if b4 != 0:
+        log4 = math.log(b4)
+
+    return log1 + log2 - log3 - log4
 
 
 # Press the green button in the guter to run the script.
@@ -43,17 +65,21 @@ if __name__ == '__main__':
     stopwords = read_file('stopwords.txt')
 
     # Tokenize text
-    token = nltk.word_tokenize(example_text)
+    tokenizer = RegexpTokenizer(r'\w+')
+    token = tokenizer.tokenize(example_text)
+    # token = nltk.word_tokenize(example_text)
 
     # Filter stopwords
     filtered_words = [w for w in token if not w.lower() in stopwords]
 
     # Remove some silly characters
     for w in filtered_words:
-        if w.__eq__('-') or w.__eq__('ß') or w.__eq__('é'):
+        try:
+            val = int(w)
             filtered_words.remove(w)
-
-    # print(filtered_words)
+        except ValueError:
+            continue
+    #print(filtered_words)
 
     # Make list of bigrams
     bigrams = nltk.bigrams(filtered_words)
@@ -79,21 +105,33 @@ if __name__ == '__main__':
         if w.__eq__(w):
             wordmap[w] = wordmap.get(w) + 1
 
-
-
-    print(wordmap)
-
-
+    #print(wordmap)
 
     # Count bigrams
-    frequence = nltk.FreqDist(bigrams)
-    for key, value in frequence.items():
-        print(key, value)
+    frequence_bigrams = nltk.FreqDist(bigrams)
+    #for key, value in frequence_bigrams.items():
+        #print(key, value)
+        #print(type(key))
 
+    # Count trigrams
+    frequence_trigrams = nltk.FreqDist(trigrams)
+
+    f = open("result.txt", "a", encoding='utf-8')
+    f.truncate(0)
     # Calc Ps
-    # Calc log
+    for t in trigrams:
+        c1 = wordmap.get(t[0])
+        c2 = wordmap.get(t[1])
+        c3 = wordmap.get(t[2])
+        bigram_t = tuple((t[0], t[1]))
+        c12 = frequence_bigrams.get(bigram_t)
+        c123 = frequence_trigrams.get(t)
+        p = c3 / wordcount
+        p1 = c123 / c12
+        p2 = (c3 - c123) / (wordcount - c12)
+        log_end = -2 * log_lambda(wordcount, c12, c123, c3, p, p1, p2)
+        f.write(t.__str__() + ',' + '(' + log_end.__str__() + ', ' + c1.__str__() + ', ' + c2.__str__() + ', ' + c3.__str__() + ')')
+        print(t.__str__() + ',' + '(' + log_end.__str__() + ', ' + c1.__str__() + ', ' + c2.__str__() + ', ' + c3.__str__() + ')')
 
-
-
-
+    f.close()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

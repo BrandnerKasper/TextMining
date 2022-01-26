5042f8e952ae576f20d64fd1ec1307a3c0688b77
s363289 <tobias.brandner@stud-mail.uni-wuerzburg.de>
2022-01-26 17:39:45 +0100

    changed create dic with last tag, curr tag and word to hold all different possibilites

--- TextMining_03/maximum_entropie.py
+++ TextMining_03/maximum_entropie.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import List, Any, Union, DefaultDict
 from collections import defaultdict
 import numpy as np
@@ -18,7 +19,7 @@ def read_file_lines(filepath) \
     return content
 
 
-def create_tag_set() \
+def create_tag_set(text) \
         -> set:
     tag_set_local = set()
     for line in text:
@@ -30,7 +31,8 @@ def create_tag_set() \
     return tag_set_local
 
 
-def create_word_label_dic():
+def create_word_label_dic(text, tag_set) \
+        -> dict[str, dict[str, int]]:
     print("\nWord : Label")
     word_label_dic_local = {}
     for line in tqdm(text):
@@ -47,54 +49,69 @@ def create_word_label_dic():
     return word_label_dic_local
 
 
-def create_word_previous_label_dic():
+# give a tagset .-.
+def create_word_previous_label_dic(text, tag_set_with_start_token) \
+        -> dict[(str, str), dict[str, int]]:
     print("\nWord - Previous Label : Label")
     word_previous_label_dic_local = {}
-    last_tag = ''
+    last_tag = "<S>"
     for line in tqdm(text):
         if line.isspace():
             last_tag = "<S>"
         else:
+            # the actual combination of last tag, current tag and word we found
             (word, tag) = line.strip().split(" ")
             if (word, last_tag) not in word_previous_label_dic_local:
                 word_previous_label_dic_local[word, last_tag] = {}
-                for t in tag_set:
+                # init all combinations of word last tag (we found) and all the different curr tags
+                for t in tag_set_with_start_token:
                     word_previous_label_dic_local[word, last_tag][t] = 0
             word_previous_label_dic_local[word, last_tag][tag] += 1
             last_tag = tag
+            # init all combinations of word plus all different last tags with all diff curr tags
+            for ltag in tag_set_with_start_token:
+                if (word, ltag) not in word_previous_label_dic_local:
+                    word_previous_label_dic_local[word, ltag] = {}
+                    for ctag in tag_set_with_start_token:
+                        word_previous_label_dic_local[word, ltag][ctag] = 0
+
     return word_previous_label_dic_local
 
 
+# possible to use for 2 different features
+def create_feature_vector_from_dic(example_dic) -> (list, dict[int, (str, str) or (str, str, str)]):
+    # feature_vector = []
+    # for (key, value) in example_dic.items():
+    #     feature_vector.append(value)
+    pass
+
+
 def learning_function():
-
-
-    return 0
+    pass
 
 
 # Main method
-if __name__ == '__main__':
-    print("Hi from Maximum Entropie :)")
-
+def main():
     # TRAIN MODEL
     # Read text
     text = read_file_lines("BIO_formals_blank.txt")
 
     # Set with all different tags
-    tag_set = create_tag_set()
-    # print(tag_set)
+    tag_set = create_tag_set(text)
+    tag_set_with_start_token = set()
+    tag_set_with_start_token.add("<S>")
+    tag_set_with_start_token |= tag_set
+    print(tag_set)
+    print(tag_set_with_start_token)
 
     # words and tags and previous
-    word_label_dic = create_word_label_dic()
-    word_previous_label_dic = create_word_previous_label_dic()
+    word_label_dic = create_word_label_dic(text, tag_set)
+    word_previous_label_dic = create_word_previous_label_dic(text, tag_set_with_start_token)
     # print(word_label_dic)
     # print(word_previous_label_dic)
 
 
-
-
-    # CLASSIFY
-
-
-
-
+if __name__ == '__main__':
+    print("Hi from Maximum Entropie :)")
+    main()
     print("End of Maximum Entropie ^^")
